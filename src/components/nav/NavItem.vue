4e3f6d247f1acbd0e988382bc3b3fd20cc1e2968
Lian <lianernesto2005@gmail.com>
2024-04-20 22:24:16 -0400

    h

--- src/components/nav/NavItem.vue
+++ src/components/nav/NavItem.vue
@@ -3,7 +3,7 @@
   
   :active-class="'dark:text-green-500 text-green-500'"
   :exact-active-class="'dark:text-indigo-500 text-indigo-500'"
-  class="block px-3 py-2 text-white rounded dark:text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+  class="block capitalize px-3 py-2 text-white rounded dark:text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
   :to="$props.route">
     <slot />
   </RouterLink>
