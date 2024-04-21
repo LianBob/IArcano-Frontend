e7f61f7ecbe0cab6031b51d67afd9292c589e449
Lian <lianernesto2005@gmail.com>
2024-04-20 22:14:44 -0400

    ghm

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grow flex flex-col items-center justify-center">
+  <div class="grow flex flex-col items-center justify-center ">
     <p class="p-4 bg-purple-400 rounded">hola mundo</p>
   </div>
 </template>
